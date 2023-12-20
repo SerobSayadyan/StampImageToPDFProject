d8623f573caab8957e130ce5fb9e597adc96263b
SerobSayadyan <serobsayadyan97@gmail.com>
2023-12-20 15:52:15 +0400

    Added function to draw required stamp image to the bottom left part of PDF file. In order to work with PDF files via java I've used 'Apache PDFBox' dependency, which you can find in pom.xml file of the project

--- src/main/java/org/example/UIForSignatureToImageConvert.java
+++ src/main/java/org/example/UIForSignatureToImageConvert.java
@@ -1,19 +1,29 @@
 package org.example;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class UIForSignatureToImageConvert {
 
-    private static final File STAMP_MODEL_NO_SIGNATURE = new File("img/Black Creative Consultant Logo.jpg");
-    private static final File STAMP_WITH_SIGNATURE = new File("img/Stamp_with_signature.jpg");
+    private static final File IMG_STAMP_MODEL_NO_SIGNATURE = new File("img/Black Creative Consultant Logo.jpg");
+    private static final File IMG_STAMP_WITH_SIGNATURE = new File("img/Stamp_with_signature.jpg");
+    private static final File DEFAULT_PDF_PATH = new File("pdfs/simple hangman.pdf");
     private static final String RED_CONSOLE_COLOR = "\u001B[31m";
+    private static final String YELLOW_CONSOLE_COLOR = "\u001B[33m";
     private static final String RESET_CONSOLE_COLOR = "\u001B[0m";
 
 
@@ -42,6 +52,7 @@ public class UIForSignatureToImageConvert {
         isTrue = true;
         do {
             System.out.println("""
+                                        
                     Do you want to specify the PDF path
                     Enter 1 or 'yes' - if yes
                     Enter 0 or 'no' - if no
@@ -50,10 +61,12 @@ public class UIForSignatureToImageConvert {
             switch (operation) {
                 case "1", "yes" -> {
                     pdfPath = enterPDFPath();
+                    imageToPDF(new File(pdfPath));
                     isTrue = false;
                 }
                 case "0", "no" -> {
                     System.out.println("Your file will be saved in - 'signatureToPDF/pdfs'\n");
+                    imageToPDF(DEFAULT_PDF_PATH);
                     isTrue = false;
                 }
                 default -> System.out.println("wrong operator");
@@ -65,17 +78,28 @@ public class UIForSignatureToImageConvert {
     private static String enterPDFPath() {
         Scanner scanner = new Scanner(System.in);
         String pdfPath = null;
-
+        Path path = null;
+        Optional<Path> optionalPath = Optional.ofNullable(path);
         boolean secondTurn = false;
         do {
             if (secondTurn) {
+                //if first time the path was incorrect the console will show WARNING
                 System.out.println(RED_CONSOLE_COLOR + " --- WARNING!!! '" + pdfPath + "' is not valid path ---" + RESET_CONSOLE_COLOR);
             }
             System.out.println("please enter valid PDF file absolute path");
             pdfPath = scanner.nextLine();
+
+            try {
+                optionalPath = Optional.ofNullable(Path.of(pdfPath));
+            } catch (InvalidPathException e) {
+                System.out.println(RED_CONSOLE_COLOR + "----WARNING--- " + e.getMessage() + RESET_CONSOLE_COLOR);
+                continue;
+            }
             secondTurn = true;
-        } while (!Files.exists(Path.of(pdfPath)));
+        } while (!Files.exists(optionalPath.orElse(Path.of("PATH/THAT/DONT/EXIST"))));
+
         return pdfPath;
+
     }
 
     private static void addSignatureToStamp(String signature) {
@@ -88,7 +112,7 @@ public class UIForSignatureToImageConvert {
         signature = sb.toString();
         try {
             // Load an existing image
-            BufferedImage image = ImageIO.read(STAMP_MODEL_NO_SIGNATURE);
+            BufferedImage image = ImageIO.read(IMG_STAMP_MODEL_NO_SIGNATURE);
 
             // Create a Graphics2D object to draw on the image
             Graphics2D g2d = image.createGraphics();
@@ -101,19 +125,69 @@ public class UIForSignatureToImageConvert {
             g2d.setColor(Color.RED);
 
             // Set the position where the text will be drawn
-            final int x = 195;
+            int x;
+            if (signature.length() == 6) {
+                x = 155;
+            } else if (signature.length() == 4) {
+                x = 190;
+            } else {
+                x = 220;
+            }
             final int y = 270;
 
             // Add your text to the image
-            g2d.drawString(signature, x, y);
+            g2d.drawString(signature.toUpperCase(), x, y);
 
             // Dispose of the Graphics2D object
             g2d.dispose();
 
             // Save the modified image
-            ImageIO.write(image, "jpg", STAMP_WITH_SIGNATURE);
+            ImageIO.write(image, "jpg", IMG_STAMP_WITH_SIGNATURE);
 
-            System.out.println("Text added to the image successfully.");
+            System.out.println(YELLOW_CONSOLE_COLOR + "Text added to the image successfully." + RESET_CONSOLE_COLOR);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void imageToPDF(final File PDF_FILE) {
+        // Load existing PDF document
+        try (PDDocument document = Loader.loadPDF(PDF_FILE)) {
+
+            // Create a new page
+            PDPage page = document.getPage(0);
+
+//            document.addPage(page);
+
+            // Load image
+            PDImageXObject image = PDImageXObject.createFromFile(IMG_STAMP_WITH_SIGNATURE.getPath(), document);
+
+            System.out.println("is e=image empty - " + image.isEmpty());
+
+            // Create content stream
+            PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.PREPEND,
+                    false, true);
+
+            // Define the position and size of the image on the page
+            float x = 500;
+            float y = 0;
+            float width = 100;
+            float height = 100;
+
+            // Draw the image on the page
+            contentStream.drawImage(image, x, y, width, height);
+
+            // Close the content stream
+            contentStream.close();
+
+            // Save the modified document
+            document.save(PDF_FILE);
+
+            // Close the document
+            document.close();
+
+            System.out.println("Image added to PDF successfully.");
 
         } catch (IOException e) {
             e.printStackTrace();
